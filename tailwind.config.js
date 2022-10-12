cf7f0d9f3e7d3ccd7a50214c1ec323778b77593c
iamzilfa <zilcyam@gmail.com>
2022-10-12 09:47:19 +0200

    all

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,15 @@ module.exports = {
         "theGray" : "#6D6E76",
         "lightGrey" :"#313852",
         "darkGrey" : "#1D2130",
-        "secondColor" : "#E0E4FC",
-        "blackcolor" : " #1D2130"
+        "secondColor" :"#E0E4FC",
+        "blackcolor" :"#1D2130",
+        "blackRock": "#2B2E3C"
       },
       backgroundImage : {
         "home-header" : "url('/img/homebrul.svg')",
+        "contact-background-image" : "url('/img/Blur\ Gradient\ \(7\).png')",
+        "component-background-image": "url('/img/Blur\ Gradient.svg')",
+        "conversions-background-image": "url('/img/Blur\ Gradient\ \(3\).svg')"
       }
     
     },
